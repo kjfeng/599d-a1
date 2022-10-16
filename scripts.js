c1c19cd606f7e8e6b089f62fc353b542aa6b852d
Kevin <kjfeng@uw.edu>
2022-10-16 01:34:21 -0700

    two columns, more edits

--- scripts.js
+++ scripts.js
@@ -2,9 +2,24 @@ $(document).ready(function() {
     $(".citation").each(function() {
         let id = $(this).attr("id");
         $(this).append(`
-            [<a class="bib" data-trigger="hover" data-toggle="popover" data-placement="top" href="#bib` + id + `" data-original-title="" title="">` + id + `</a>]
-        `);
+            [<a class="bib" data-trigger="hover" data-toggle="popover" data-placement="top" href="#bib` + id + `" data-original-title="" title="">` + id + `</a>]`);
     });
 
-    $("section > h3").append("<hr/>");
+    $(".fn").click(function() {
+        let id = $(this).attr("id");
+        let fnClass = "." + id;
+        // highlight and fade fnclass element 
+        $(fnClass).css({backgroundColor: "#ffde0a"}).animate({backgroundColor: 'transparent'}, 1500);
+        return false;
+    });
+
+    $(".fn-img").click(function() {
+        let id = $(this).attr("id");
+        let fnClass = "." + id;
+        // highlight and fade fnclass element 
+        $(fnClass).css({backgroundColor: "#ffde0a"}).animate({backgroundColor: 'transparent'}, 1500);
+        return false;
+    });
+
+    // $("section > h3").append("<hr/>");
 });
